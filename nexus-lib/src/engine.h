20964520552caa6e411ec21a7ca33461a4f4830f
Johan Rönnkvist <johan.ronnkvist@gmail.com>
2019-03-07 21:24:33 +0100

    Added key_codes and keyboard implementation

--- nexus-lib/src/engine.h
+++ nexus-lib/src/engine.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "sfml/window.h"
+#include "sfml/input.h"
+
+#include "keyboard.h"
 
 namespace nexus {
 using window = sfml::window;
+using keyboard = concrete_keyboard<sfml::key_code, sfml::keyboard>;
 }
