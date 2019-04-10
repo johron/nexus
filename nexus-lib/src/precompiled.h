4d9922205bcfc9e2fd3bbe2d2ad5afbc252e1b32
Johan Rönnkvist <johan.ronnkvist@gmail.com>
2019-04-10 22:04:58 +0200

    Disable warning for useless cast inside SFML

--- nexus-lib/src/precompiled.h
+++ nexus-lib/src/precompiled.h
@@ -15,6 +15,10 @@
 
 #include <string>
 
-#include "sfml/util.h"
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#pragma GCC diagnostic push
+#include "SFML/Graphics.hpp"
+#pragma GCC diagnostic pop
 
+#include "sfml/util.h"
 #include "util/type_id.h"
