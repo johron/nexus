189350b887187653b04ace55acc04904e494c6bd
Johan Rönnkvist <johan.ronnkvist@gmail.ccom>
2020-02-15 15:23:54 +0100

    Extended event broker to accept func ptr to object

--- nexus-core-tests/src/event_broker.cpp
+++ nexus-core-tests/src/event_broker.cpp
@@ -3,32 +3,55 @@
 
 using namespace nexus;
 
-struct test_event_1 {
+struct test_event {
 	int m_number;
 };
+struct test_listener {
+	uint32_t m_msg_count = 0;
+	void on_event(const test_event&) {
+		++m_msg_count;
+	}
+};
 
-TEST(event_broker, register_listener) {
+TEST(event_broker, register_listener_func) {
 	event_broker broker;
-	const auto token = broker.register_listener<test_event_1>([](auto& /*event*/) {});
-	EXPECT_EQ(broker.listener_count<test_event_1>(), 1u);
+	const auto token = broker.register_listener<test_event>([](auto& /*event*/) {});
+	EXPECT_EQ(broker.listener_count<test_event>(), 1u);
 }
 
-TEST(event_broker, publish_event) {
+TEST(event_broker, register_listener_object) {
+	event_broker broker;
+	test_listener listener;
+	const auto token = broker.register_listener<test_event>(&test_listener::on_event, &listener);
+	EXPECT_EQ(broker.listener_count<test_event>(), 1u);
+}
+
+TEST(event_broker, publish_event_to_func) {
 	event_broker broker;
 	uint32_t message_count(0);
 	const auto token =
-		broker.register_listener<test_event_1>([&message_count](auto& /*event*/) { ++message_count; });
+		broker.register_listener<test_event>([&message_count](auto& /*event*/) { ++message_count; });
 	EXPECT_EQ(message_count, 0u);
-	broker.publish(test_event_1{1});
+	broker.publish(test_event{1});
 	EXPECT_EQ(message_count, 1u);
+}
+
+TEST(event_broker, publish_event_to_object) {
+	event_broker broker;
+	test_listener listener;
+	const auto token = broker.register_listener<test_event>(&test_listener::on_event, &listener);
+	EXPECT_EQ(broker.listener_count<test_event>(), 1u);
+	EXPECT_EQ(listener.m_msg_count, 0u);
+	broker.publish(test_event{});
+	EXPECT_EQ(listener.m_msg_count, 1u);
 }
 
 TEST(event_broker, enqueue_event) {
 	event_broker broker;
 	uint32_t message_count(0);
 	const auto token =
-		broker.register_listener<test_event_1>([&message_count](auto& /*event*/) { ++message_count; });
-	broker.enqueue(test_event_1{1});
+		broker.register_listener<test_event>([&message_count](auto& /*event*/) { ++message_count; });
+	broker.enqueue(test_event{1});
 	EXPECT_EQ(message_count, 0u);
 	broker.flush();
 	EXPECT_EQ(message_count, 1u);
@@ -39,9 +62,9 @@ TEST(event_broker, expired_listener_token_removes_listener) {
 	uint32_t message_count(0);
 	{
 		auto token =
-			broker.register_listener<test_event_1>([&message_count](auto& /*event*/) { ++message_count; });
+			broker.register_listener<test_event>([&message_count](auto& /*event*/) { ++message_count; });
 	}
-	broker.publish(test_event_1{1});
+	broker.publish(test_event{1});
 	EXPECT_EQ(message_count, 0u);
 }
 
@@ -50,41 +73,35 @@ TEST(event_broker, listener_token_can_be_copied) {
 	uint32_t message_count(0);
 	{
 		auto first_token =
-			broker.register_listener<test_event_1>([&message_count](auto& /*event*/) { ++message_count; });
+			broker.register_listener<test_event>([&message_count](auto& /*event*/) { ++message_count; });
 		auto second_token(first_token);
 	}
-	broker.publish(test_event_1{1});
+	broker.publish(test_event{1});
 	EXPECT_EQ(message_count, 0u);
 }
 
 TEST(event_broker, listener_token_can_expire_after_system) {
 	auto broker = std::make_unique<event_broker>();
-	auto first_token = broker->register_listener<test_event_1>([](auto& /*event*/) {});
+	auto first_token = broker->register_listener<test_event>([](auto& /*event*/) {});
 	broker.reset();
 }
 
 TEST(event_broker, invalidated_listener_token_stops_events) {
 	event_broker broker;
 	uint32_t message_count(0);
-	auto token = broker.register_listener<test_event_1>([&message_count](auto& /*event*/) { ++message_count; });
+	auto token = broker.register_listener<test_event>([&message_count](auto& /*event*/) { ++message_count; });
 	auto tokenCopy(token); // even if a copy exists, invalidating the listener token will stop all events
-	broker.publish(test_event_1{1});
+	broker.publish(test_event{1});
 	EXPECT_EQ(message_count, 1u);
 	token.invalidate();
-	broker.publish(test_event_1{1});
+	broker.publish(test_event{1});
 	EXPECT_EQ(message_count, 1u);
 }
 
-struct test_msg {};
-struct test_listener {
-	uint32_t m_msg_count = 0;
-	void on_msg(const test_msg&) { ++m_msg_count; }
-};
-
-TEST(event_broker, publish_to_target) {
+TEST(event_broker, publish_to_single_target) {
 	event_broker broker;
 	test_listener listener;
 	EXPECT_EQ(listener.m_msg_count, 0u);
-	broker.publish(test_msg{}, &test_listener::on_msg, listener);
+	broker.publish(test_event{}, &test_listener::on_event, &listener);
 	EXPECT_EQ(listener.m_msg_count, 1u);	
 }
--- nexus-core/src/event_broker.h
+++ nexus-core/src/event_broker.h
@@ -11,7 +11,9 @@ struct event_broker {
 			, m_event_id(event_id)
 			, m_listener_id(listener_id) {}
 
-		~cancel_token() { detach(); }
+		~cancel_token() {
+			detach();
+		}
 
 		void detach() {
 			if (m_system) {
@@ -20,7 +22,9 @@ struct event_broker {
 			}
 		}
 
-		[[nodiscard]] bool is_detached() const { return m_system == nullptr; }
+		[[nodiscard]] bool is_detached() const {
+			return m_system == nullptr;
+		}
 
 	private:
 		event_broker* m_system;
@@ -32,9 +36,13 @@ struct event_broker {
 		listener_token(std::shared_ptr<cancel_token> token)
 			: m_token(token) {}
 
-		listener_token(const listener_token& other) { *this = other; }
+		listener_token(const listener_token& other) {
+			*this = other;
+		}
 
-		listener_token(listener_token&& other) { *this = std::forward<listener_token>(other); }
+		listener_token(listener_token&& other) {
+			*this = std::forward<listener_token>(other);
+		}
 
 		listener_token& operator=(const listener_token& other) {
 			m_token = other.m_token;
@@ -52,14 +60,18 @@ struct event_broker {
 			}
 		}
 
-		[[nodiscard]] bool is_valid() const { return m_token && !m_token->is_detached(); }
+		[[nodiscard]] bool is_valid() const {
+			return m_token && !m_token->is_detached();
+		}
 
 	private:
 		std::shared_ptr<cancel_token> m_token;
 	};
 
 	event_broker() = default;
-	~event_broker() { detach_all(); }
+	~event_broker() {
+		detach_all();
+	}
 
 	event_broker(const event_broker& other) = delete;
 	event_broker(event_broker&& other) = delete;
@@ -73,6 +85,11 @@ struct event_broker {
 		const auto token = std::make_shared<cancel_token>(this, event_id, listener_id);
 		m_listeners[event_id][listener_id] = listener_data{func, token};
 		return listener_token{token};
+	}
+
+	template <class event_t, class function_ptr_t, class target_t>
+	[[nodiscard]] listener_token register_listener(const function_ptr_t& function_ptr, target_t* target) {
+		return register_listener<event_t>([target, function_ptr](const event_t& event) { (target->*function_ptr)(event); });
 	}
 
 	void remove_listener(std::type_index event_id, uint32_t listener_id) {
@@ -96,10 +113,9 @@ struct event_broker {
 	}
 
 	template <class event_t, class function_ptr_t, class target_t>
-	void publish(const event_t& event, const function_ptr_t& function_ptr, target_t& target) {
-		(target.*function_ptr)(event);
+	void publish(const event_t& event, const function_ptr_t& function_ptr, target_t* target) {
+		(target->*function_ptr)(event);
 	}
-
 
 	template <class event_t>
 	void enqueue(event_t&& event) {
